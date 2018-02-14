b8e62e234d0ad2e4e46ad57e09725f490dff48b2
Luis Lopez <delucholuis@gmail.com>
2018-02-13 17:07:10 -0800

    Installed Packages

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    React.createElement(
+      'table',
+      null,
+      React.createElement(
+        'tr',
+        null,
+        React.createElement(
+          'th',
+          null,
+          'First Name', " ", 'Last Name',
+          React.createElement('td', null, 'Janet', " " , 'James')
+        )
+      ),
+      React.createElement(
+        'table',
+        null,
+        React.createElement(
+          'tr',
+          null,
+          React.createElement(
+              'th',
+              null,
+              'Last Name',
+          React.createElement('td', null, 'John', " " , 'Jameson')
+          )
+        )
+      )
+    ),
+    document.getElementById('root')
+  );
+
+// ReactDOM.render(
+//     React.createElement(
+//         'li',
+//         null,
+//         'Item 1',
+//         React.createElement(
+//           'ul',
+//           null,
+//           React.createElement('li', null, 'Price 1')
+//         )
+//       ),
+//       React.createElement(
+//         'li',
+//         null,
+//         'Item 2',
+//         React.createElement(
+//           'ul',
+//           null,
+//           React.createElement('li', null, 'Price 2')
+
+//             )
+//         )
+//     ),
+//     document.getElementById('root')
+// )
